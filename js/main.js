feb14cdad1bf6fd3984bee8a860040362e75b643
lily <919931669@qq.com>
2016-10-12 10:19:00 +0800

    all

---
+++ js/main.js
@@ -0,0 +1,11 @@
+﻿$(function(){
+	var inithei=$(window).height();
+	var dochei=$(document).height();
+	$("#mask").css({height:dochei})
+	
+	$(".j-close").on("click",function(){
+		$(this).parents(".pop-packet").fadeOut();
+		$("#mask").fadeOut();
+	})
+})
+
